15556a074b119f19b6e6ea4d85771a046012d286
Leeyum28 <biz4liam@yahoo.com>
2023-07-23 23:43:39 -0400

    Adding a new function to the code

--- SQLProj.py
+++ SQLProj.py
@@ -1,26 +1,46 @@
 import sqlite3
 
-connection = sqlite3.connect("lite.db")
-cursor = connection.cursor()
+
 
 def create_table():
+    connection = sqlite3.connect("lite.db")
+    cursor = connection.cursor()
     cursor.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INTEGER, price REAL)")
     connection.commit()
     connection.close()
 
 def insert_table(item, quantity, price):
-    cursor.execute("INSERT INTO store VALUES (?,?,?)",(item, quantity, price))
+    connection = sqlite3.connect("lite.db")
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM store")
+    for item in cursor.fetchall():
+        counter = 1
+        itemname = item[0]
+        list_of_items = []
+        list_of_items += itemname
+        complete_list = ''.join(list_of_items)
+        if complete_list[0] != complete_list[1]:
+            cursor.execute("INSERT INTO store VALUES (?,?,?)",(item, quantity, price))
+    connection.commit()
+    connection.close()
 
-def delete_data():
-    cursor.execute("DELETE FROM store WHERE item='Hennesy'")
+def delete_data(item):
+    connection = sqlite3.connect("lite.db")
+    cursor = connection.cursor()
+    cursor.execute("DELETE FROM store WHERE item=?",(item,))
+    connection.commit()
+    connection.close()
 
-delete_data()
-insert_table("PROPER TWELVE", 8, 29.99)
+#delete_data('HENNESSEY')
+insert_table("PROPER TWELVE", 3, 38.99)
 
 def view_table():
+    connection = sqlite3.connect("lite.db")
+    cursor = connection.cursor()
     cursor.execute("SELECT * FROM store")
     rows = cursor.fetchall()
+    connection.commit()
+    connection.close()
     return rows
 
-print(view_table())
-connection.close()
+#print(view_table())
